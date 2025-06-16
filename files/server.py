aae15c48018f91bb01b60d20dfa00b7cb293407b
koolaidman129 <127859176+koolaidman129@users.noreply.github.com>
2025-06-16 08:20:18 -0400

    forgot what i added but i'm pushing before step 3 'llm client'

--- files/server.py
+++ files/server.py
@@ -1,15 +1,30 @@
 # server.py
 from mcp.server.fastmcp import FastMCP
+import requests
 
 # Create an MCP server
 mcp = FastMCP("Demo")
-
 
 # Add an addition tool
 @mcp.tool()
 def add(a: int, b: int) -> int:
     return a + b
 
+@mcp.tool()
+def kanye_quote() -> str:
+    url = "https://api.kanye.rest/"
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            posts = response.json()
+            return posts
+        else:
+            
+            return f"Error: , {response.status_code}"
+    except requests.exceptions.RequestException as e:
+        return f"Error: , {e}"
 
 # Add a dynamic greeting resource
 @mcp.resource("greeting://{name}")
